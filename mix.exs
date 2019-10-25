ce5b289ff47f3a17747acffdb09ae5c13d0d4f25
Davide Bettio <davide.bettio@ispirata.com>
2019-10-25 17:54:30 +0200

    Get ready for hex.pm
    Add all required configuration entries such as package, so this library can
    be published to hex.pm.

--- mix.exs
+++ mix.exs
@@ -6,20 +6,36 @@ defmodule PrettyLog.MixProject do
       app: :pretty_log,
       version: "0.1.0",
       elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/ispirata/pretty_log"
     ]
   end
 
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   defp deps do
     [
-      {:logfmt, "~> 3.3"}
+      {:logfmt, "~> 3.3"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "An Elixir log formatter library."
+  end
+
+  defp package do
+    [
+      maintainers: ["Davide Bettio"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/ispirata/pretty_log",
+        "Documentation" => "http://hexdocs.pm/pretty_log/"
+      }
     ]
   end
 end
