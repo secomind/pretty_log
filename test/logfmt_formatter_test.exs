415c122809eaf1be28f7b7d27932830f165665e4
Oliver Marriott <hello@omarriott.com>
2021-04-07 03:08:56 +1000

    LogfmtFormatterTest: Add key name configuration tests

--- test/logfmt_formatter_test.exs
+++ test/logfmt_formatter_test.exs
@@ -10,6 +10,7 @@ defmodule PrettyLog.LogfmtFormatterTest do
 
   def local_tz_offset(time) do
     {date, {h, m, s, millis}} = time
+
     timestamp =
       {date, {h, m, s}}
       |> :erlang.localtime_to_universaltime()
@@ -73,5 +74,71 @@ defmodule PrettyLog.LogfmtFormatterTest do
            ) ==
              "level=debug ts=2019-10-08T11:58:39.005+#{local_tz_offset(@time)} msg=\"This is a test message\" " <>
                "kwlist=\"base64-encoded-ext-term:g2wAAAACaAJkAAFhYQFoAmQAAWJhAmo=\"\n"
+  end
+
+  describe "level key name configuration" do
+    setup do
+      Application.put_env(:pretty_log, :level_key_name, :severity)
+
+      on_exit(fn ->
+        Application.delete_env(:pretty_log, :level_key_name)
+      end)
+    end
+
+    test "sets level key" do
+      assert :erlang.iolist_to_binary(
+               LogfmtFormatter.format(
+                 :warn,
+                 "This is a test message",
+                 @time,
+                 []
+               )
+             ) ==
+               "severity=warn ts=2019-10-08T11:58:39.005+#{local_tz_offset(@time)} msg=\"This is a test message\"\n"
+    end
+  end
+
+  describe "timestamp name configuration" do
+    setup do
+      Application.put_env(:pretty_log, :timestamp_key_name, :event_at)
+
+      on_exit(fn ->
+        Application.delete_env(:pretty_log, :timestamp_key_name)
+      end)
+    end
+
+    test "sets timestamp key" do
+      assert :erlang.iolist_to_binary(
+               LogfmtFormatter.format(
+                 :warn,
+                 "This is a test message",
+                 @time,
+                 []
+               )
+             ) ==
+               "level=warn event_at=2019-10-08T11:58:39.005+#{local_tz_offset(@time)} msg=\"This is a test message\"\n"
+    end
+  end
+
+  describe "message name configuration" do
+    setup do
+      Application.put_env(:pretty_log, :message_key_name, :note_to_human)
+
+      on_exit(fn ->
+        Application.delete_env(:pretty_log, :message_key_name)
+      end)
+    end
+
+    test "sets message key" do
+      assert :erlang.iolist_to_binary(
+               LogfmtFormatter.format(
+                 :warn,
+                 "This is a test message",
+                 @time,
+                 []
+               )
+             ) ==
+               "level=warn ts=2019-10-08T11:58:39.005+#{local_tz_offset(@time)} note_to_human=\"This is a test message\"\n"
+    end
   end
 end
