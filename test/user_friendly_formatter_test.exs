b55a4c75e811cd5d04a4bfcd4fdc390f09af90f1
Davide Bettio <davide.bettio@ispirata.com>
2021-04-21 12:09:48 +0200

    UserFriendlyFormatter: 100% coverage
    Add tests for messages with metadata and crashes.

--- test/user_friendly_formatter_test.exs
+++ test/user_friendly_formatter_test.exs
@@ -13,4 +13,21 @@ defmodule PrettyLog.UserFriendlyFormatterTest do
              )
            ) == "11:58:39.5\t|WARN | This is a test message\n"
   end
+
+  test "formats an error log entry with an integer metadata" do
+    assert :erlang.iolist_to_binary(
+             UserFriendlyFormatter.format(
+               :error,
+               "This is a test message",
+               {{2019, 10, 8}, {11, 58, 39, 5}},
+               line: 100
+             )
+           ) ==
+             "11:58:39.5\t|ERROR| This is a test message  line=100\n"
+  end
+
+  test "format does not crash" do
+    assert :erlang.iolist_to_binary(UserFriendlyFormatter.format(:fail, "hello", :err, [])) ==
+             "LOG_FORMATTER_ERROR: {:fail, \"hello\", :err, []}\n"
+  end
 end
